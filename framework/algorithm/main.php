4128810dbb01779b9561c6127b046952fd5facc0
jinchengchen <326550324@qq.com>
2017-09-08 14:19:40 +0800

    去掉了array_column算法

--- framework/algorithm/main.php
+++ framework/algorithm/main.php
@@ -1,22 +1,5 @@
 <?php
 /**
- * 获取一个数组的某一列
- * 返回数字下标的数组
- * @param array $array
- * @param string|int $column
- * @return array
- */
-function getArrayColumn($array,$column)
-{
-	$temp = array();
-	foreach ($array as $value)
-	{
-		$temp[] = $value[$column];
-	}
-	return $temp;
-}
-
-/**
  * 快速排序算法
  * 返回数据所在的key
  * @param array $array
