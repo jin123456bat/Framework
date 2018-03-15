37e21fd6446b2f5921705ccd7530884722c4996c
jinchengchen <326550324@qq.com>
2018-03-15 15:47:32 +0800

    sphinx

---
+++ core/database/driver/sphinx.php
@@ -0,0 +1,128 @@
+<?php
+namespace framework\core\database\driver;
+use framework\core\database\database;
+
+class sphinx extends database
+{
+	/**
+	 * {@inheritDoc}
+	 * @see \framework\core\database\database::query()
+	 */
+	public function query($sql, array $array = array())
+	{
+		// TODO 自动生成的方法存根
+		
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see \framework\core\database\database::execute()
+	 */
+	public function execute($sql, array $array = array())
+	{
+		// TODO 自动生成的方法存根
+		
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see \framework\core\database\database::fetch()
+	 */
+	public function fetch($sql, array $array = array(), $callback = NULL)
+	{
+		// TODO 自动生成的方法存根
+		
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see \framework\core\database\database::setConfig()
+	 */
+	public function setConfig($name, $value)
+	{
+		// TODO 自动生成的方法存根
+		
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see \framework\core\database\database::getConfig()
+	 */
+	public function getConfig($name = NULL)
+	{
+		// TODO 自动生成的方法存根
+		
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see \framework\core\database\database::error()
+	 */
+	public function error()
+	{
+		// TODO 自动生成的方法存根
+		
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see \framework\core\database\database::errno()
+	 */
+	public function errno()
+	{
+		// TODO 自动生成的方法存根
+		
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see \framework\core\database\database::showTables()
+	 */
+	public function showTables()
+	{
+		// TODO 自动生成的方法存根
+		
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see \framework\core\database\database::transaction()
+	 */
+	public function transaction()
+	{
+		// TODO 自动生成的方法存根
+		
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see \framework\core\database\database::commit()
+	 */
+	public function commit()
+	{
+		// TODO 自动生成的方法存根
+		
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see \framework\core\database\database::rollback()
+	 */
+	public function rollback()
+	{
+		// TODO 自动生成的方法存根
+		
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see \framework\core\database\database::inTransaction()
+	 */
+	public function inTransaction()
+	{
+		// TODO 自动生成的方法存根
+		
+	}
+
+	
+}
---
+++ core/database/sphinx/index.php
@@ -0,0 +1,8 @@
+<?php
+namespace framework\core\database\sphinx;
+use framework\core\base;
+
+class index extends base
+{
+	
+}
